bd76ef2c6b5ab72b8413fca6ca64295dcad4317e
braianorl <132423875+braianorl@users.noreply.github.com>
2025-06-06 14:26:51 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Configuração do Firebase para o jogo da memória em mandarim
+const firebaseConfig = {
+  apiKey: "AIzaSyDgxnFuGUj2RYUrBiNDJU-YUvM8SgoEKJo",
+  authDomain: "jogo-memoria-mandarim.firebaseapp.com",
+  projectId: "jogo-memoria-mandarim",
+  databaseURL: "https://jogo-memoria-mandarim-default-rtdb.firebaseio.com",
+  storageBucket: "jogo-memoria-mandarim.firebasestorage.app",
+  messagingSenderId: "682946139326",
+  appId: "1:682946139326:web:b1d8eda8cf89a10d867aef",
+  measurementId: "G-5TH9F9KZ45"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
